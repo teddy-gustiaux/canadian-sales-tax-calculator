f92228907a4961e5f5ad6da0899ff213f336f9f8
Teddy Gustiaux <teddy.gustiaux@gmail.com>
2017-03-15 16:53:19 -0400

    Added basic percentage calculator

--- app/src/main/java/com/gustiaux/canadiansalestaxcalculator/MainActivity.java
+++ app/src/main/java/com/gustiaux/canadiansalestaxcalculator/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +24,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        result = (TextView) findViewById(R.id.result);
+        EditText text = (EditText) findViewById(R.id.priceInput);
+        text.addTextChangedListener(inputPriceWatcher);
     }
 
     @Override
@@ -49,4 +50,21 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private TextWatcher inputPriceWatcher = new TextWatcher() {
+
+        public void afterTextChanged(Editable s) {
+            result.setText("1.0");
+            Double inputPrice = Double.parseDouble(s.toString());
+            inputPrice = inputPrice * 1.13;
+            result.setText(inputPrice.toString());
+        }
+
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before,
+                                  int count) {
+        }
+    };
 }
