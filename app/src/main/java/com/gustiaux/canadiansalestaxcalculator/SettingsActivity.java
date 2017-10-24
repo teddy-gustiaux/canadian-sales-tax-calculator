d7908e1b863c92da99943585657e8876e58d27ca
Unknown <dev@gustiaux.com>
2017-10-24 15:44:13 -0400

    Fix bug with arrow navigation

--- app/src/main/java/com/gustiaux/canadiansalestaxcalculator/SettingsActivity.java
+++ app/src/main/java/com/gustiaux/canadiansalestaxcalculator/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.gustiaux.canadiansalestaxcalculator;
 
 
 import android.annotation.TargetApi;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -114,8 +115,19 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            if (!super.onMenuItemSelected(featureId, item)) {
-                NavUtils.navigateUpFromSameTask(this);
+            Intent upIntent = NavUtils.getParentActivityIntent(this);
+            if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+                // This activity is NOT part of this app's task, so create a new task
+                // when navigating up, with a synthesized back stack.
+                TaskStackBuilder.create(this)
+                        // Add all of this activity's parents to the back stack
+                        .addNextIntentWithParentStack(upIntent)
+                        // Navigate up to the closest parent
+                        .startActivities();
+            } else {
+                // This activity is part of this app's task, so simply
+                // navigate up to the logical parent activity.
+                NavUtils.navigateUpTo(this, upIntent);
             }
             return true;
         }
